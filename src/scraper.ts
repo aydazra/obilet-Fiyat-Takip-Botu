fa4402af768cebd3b41e4fa66a0af0d8734fa51d
AZRA AYDINER <azra_aydiner@hotmail.com>
2026-03-02 14:28:17 +0300

    Update scraper.ts

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,4 +1,5 @@
-import puppeteer, { Browser, Page } from "puppeteer";
+import puppeteer from "puppeteer";
+import type { Browser, Page } from "puppeteer";
 import { Flight, SearchConfig, ScrapeResult } from "./interfaces";
 
 // Türkçe normalize
